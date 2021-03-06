fd8127daa5d6be67eabbde5f30bc4deff6473b7d
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-03-06 20:18:11 +0100

    board is now a class field, scanner added

--- src/app/tictac/TicTacToe.java
+++ src/app/tictac/TicTacToe.java
@@ -1,15 +1,17 @@
 package app.tictac;
 
+import java.util.Scanner;
 
 class TicTacToe {
+    private static Scanner scanner = new Scanner(System.in);
+    private static char[][] board = {{' ', '|', ' ', '|', ' '},
+            {'-', '+', '-', '+', '-'},
+            {' ', '|', ' ', '|', ' '},
+            {'-', '+', '-', '+', '-'},
+            {' ', '|', ' ', '|', ' '}
+    };
 
-    private static void printBoard() {
-        char[][] board = {{' ', '|', ' ', '|', ' '},
-                {'-', '+', '-', '+', '-'},
-                {' ', '|', ' ', '|', ' '},
-                {'-', '+', '-', '+', '-'},
-                {' ', '|', ' ', '|', ' '}
-        };
+    private static void printBoard(char[][] board) {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
                 System.out.print(board[i][j]);
@@ -17,9 +19,14 @@ class TicTacToe {
             System.out.println();
         }
     }
-
+//        [00],[02],[04]
+//        [20],[22],[24]
+//        [40],[42],[44]
 
     public static void main(String[] args) {
-        printBoard();
+        printBoard(board);
+        int player =scanner.nextInt();
+
+        printBoard(board);
     }
 }
