474c380df56b73f793d4dd51e6f2ae7e1119c800
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-03-06 19:51:27 +0100

    method to print a board created

---
+++ src/app/tictac/TicTacToe.java
@@ -0,0 +1,25 @@
+package app.tictac;
+
+
+class TicTacToe {
+
+    private static void printBoard() {
+        char[][] board = {{' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '}
+        };
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                System.out.print(board[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        printBoard();
+    }
+}
--- src/java/tictac/TicTacToe.java
+++
@@ -1,9 +0,0 @@
-package java.tictac;
-
-
-
-class TicTacToe {
-    public static void main(String[] args) {
-
-    }
-}
