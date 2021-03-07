631765e45d8ab069ed41886bb849d72e6096de45
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-03-07 20:43:37 +0100

    aiMove method changed-now its filling only empty fields

--- src/app/tictac/TicTacToe.java
+++ src/app/tictac/TicTacToe.java
@@ -61,35 +61,39 @@ class TicTacToe {
     private static void aiMove(char[][] board) {
         Random random = new Random();
         int move = random.nextInt(9) + 1;
-        if (move == 1) {
-            board[0][0] = 'O';
+        if (!usedNumbers.contains(move)) {
+            if (move == 1) {
+                board[0][0] = 'O';
+            }
+            if (move == 2) {
+                board[0][2] = 'O';
+            }
+            if (move == 3) {
+                board[0][4] = 'O';
+            }
+            if (move == 4) {
+                board[2][0] = 'O';
+            }
+            if (move == 5) {
+                board[2][2] = 'O';
+            }
+            if (move == 6) {
+                board[2][4] = 'O';
+            }
+            if (move == 7) {
+                board[4][0] = 'O';
+            }
+            if (move == 8) {
+                board[4][2] = 'O';
+            }
+            if (move == 9) {
+                board[4][4] = 'O';
+            }
+            numberOfMoves++;
+            usedNumbers.add(move);
+        } else {
+            aiMove(board);
         }
-        if (move == 2) {
-            board[0][2] = 'O';
-        }
-        if (move == 3) {
-            board[0][4] = 'O';
-        }
-        if (move == 4) {
-            board[2][0] = 'O';
-        }
-        if (move == 5) {
-            board[2][2] = 'O';
-        }
-        if (move == 6) {
-            board[2][4] = 'O';
-        }
-        if (move == 7) {
-            board[4][0] = 'O';
-        }
-        if (move == 8) {
-            board[4][2] = 'O';
-        }
-        if (move == 9) {
-            board[4][4] = 'O';
-        }
-        numberOfMoves++;
-        usedNumbers.add(move);
     }
 
     public static void playAGame() {
